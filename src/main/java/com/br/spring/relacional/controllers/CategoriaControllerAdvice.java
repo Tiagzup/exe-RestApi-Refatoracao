cb19e5ca9772b3aeed0d6b62deb182144036d295
Tiago Souza <tiago.souza@zup.com.br>
2019-07-30 16:12:00 -0300

    Refatoração restApi

--- src/main/java/com/br/spring/relacional/controllers/CategoriaControllerAdvice.java
+++ src/main/java/com/br/spring/relacional/controllers/CategoriaControllerAdvice.java
@@ -1,5 +1,10 @@
 package com.br.spring.relacional.controllers;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
 public class CategoriaControllerAdvice {
 
 }
